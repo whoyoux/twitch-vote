4e52d9daad2a6cf628ddaf8f933025f7337c4b4d
whoyoux <48690153+whoyoux@users.noreply.github.com>
2023-11-07 14:54:11 +0100

    init

---
+++ src/components/header.tsx
@@ -0,0 +1,26 @@
+import ThemeSwitcher from "./theme-switcher";
+import { Button } from "./ui/button";
+
+const Header = () => {
+  return (
+    <header className="w-full max-w-full flex items-center justify-between py-10 border-b">
+      <h1 className="hidden sm:block scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+        twitch voter
+      </h1>
+      <h1 className="sm:hidden scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+        logo
+      </h1>
+
+      <div className="flex items-center gap-4">
+        <Account />
+        <ThemeSwitcher />
+      </div>
+    </header>
+  );
+};
+
+const Account = () => {
+  return <Button size="lg">Login via Twitch</Button>;
+};
+
+export default Header;
---
+++ src/lib/auth.ts
@@ -0,0 +1,12 @@
+import type { NextAuthOptions } from "next-auth";
+
+import TwitchProvider from "next-auth/providers/twitch";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    TwitchProvider({
+      clientId: process.env.TWITCH_CLIENT_ID!,
+      clientSecret: process.env.TWITCH_CLIENT_SECRET!,
+    }),
+  ],
+};
